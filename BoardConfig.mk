bf9366da3068797e1fbf85766d5f31917771444c
Nyks45 <winglow45@gmail.com>
2016-10-12 18:53:19 +0300

    Revert MM Commits
    This Commit Reverts
    https://github.com/AICP/device_asus_Z008/commit/09390404d55fc0591f9975283daac869c4d23459
    https://github.com/AICP/device_asus_Z008/commit/793003d7c48b9561fff307f1afc0cc712dfa37ae
    https://github.com/AICP/device_asus_Z008/commit/1b6763a793b4f7acebc03ba3d2ce217ecc43c0ed
    https://github.com/AICP/device_asus_Z008/commit/4e6e3f33d937bab7ba317f933efa65a4d51b2063
    https://github.com/AICP/device_asus_Z008/commit/9ff86bbd0793583f16126fd753351e79b0071ca4

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -17,18 +17,10 @@
 # inherit from common mofd-common
 -include device/asus/mofd-common/BoardConfigCommon.mk
 
-# Charger
-BOARD_HEALTHD_CUSTOM_CHARGER_RES := device/asus/Z008/charger/images
-
-# Mkbootimg
-BOARD_BOOT_KEY := device/asus/Z008/keys/boot_z008.sig
-BOARD_RECOVERY_KEY := device/asus/Z008/keys/recovery_z008.sig
+TARGET_RELEASETOOLS_EXTENSIONS := device/asus/Z008
 
 # Partitions
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 12478019584
-
-# Require Ifwi version
-TARGET_BOARD_INFO_FILE ?= device/asus/Z008/board-info.txt
 
 # Use the non-open-source parts, if they're present
 -include vendor/asus/Z008/BoardConfigVendor.mk
